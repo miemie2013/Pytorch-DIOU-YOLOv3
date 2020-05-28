5e3be39f407aae41208f4f226e89bb8028861b0c
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2020-05-28 15:16:12 +0800

    fix nan

--- model/darknet_yolo_pt.py
+++ model/darknet_yolo_pt.py
@@ -52,7 +52,7 @@ def bbox_ciou(boxes1, boxes2):
     inter_section = T.where(inter_section < 0.0, inter_section*0, inter_section)
     inter_area = inter_section[..., 0] * inter_section[..., 1]
     union_area = boxes1_area + boxes2_area - inter_area
-    iou = inter_area / union_area
+    iou = inter_area / (union_area + 1e-9)
 
     # 包围矩形的左上角坐标、右下角坐标，shape 都是 (8, 13, 13, 3, 2)
     enclose_left_up = T.min(boxes1_x0y0x1y1[..., :2], boxes2_x0y0x1y1[..., :2])
@@ -65,10 +65,9 @@ def bbox_ciou(boxes1, boxes2):
     # 两矩形中心点距离的平方
     p2 = T.pow(boxes1[..., 0] - boxes2[..., 0], 2) + T.pow(boxes1[..., 1] - boxes2[..., 1], 2)
 
-    # 增加av。分母boxes2[..., 3]可能为0，所以加上除0保护防止nan。
-    atan1 = T.atan(boxes1[..., 2] / boxes1[..., 3])
-    temp_a = T.where(boxes2[..., 3] > 0.0, boxes2[..., 3], boxes2[..., 3] + 1.0)
-    atan2 = T.atan(boxes2[..., 2] / temp_a)
+    # 增加av。加上除0保护防止nan。
+    atan1 = T.atan(boxes1[..., 2] / (boxes1[..., 3] + 1e-9))
+    atan2 = T.atan(boxes2[..., 2] / (boxes2[..., 3] + 1e-9))
     v = 4.0 * T.pow(atan1 - atan2, 2) / (math.pi ** 2)
     a = v / (1 - iou + v)
 
